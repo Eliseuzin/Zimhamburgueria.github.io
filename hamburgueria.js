8aeb56d5663fc571f6c9406a65833cc2da4d6ea1
Eliseu <eliseumatos321@gmail.com>
2024-10-24 20:26:31 -0300

    add conteudos

---
+++ hamburgueria.js
@@ -0,0 +1,24 @@
+const Menugeral = document.getElementById("menu");
+console.log(Menugeral);
+const Meucarrinho = document.getElementById("meucarrinho");
+const dentrodocarrinho = document.getElementById("dentrodocarrinho");
+const Valortotal = document.getElementById("valortotal");
+const Fechar = document.getElementById("Fechar");
+const Finalizar = document.getElementById("Finalizar");
+const Quantidadecarinho = document.getElementById("quantidadecarinho");
+const Addressinput = document.getElementById("address");
+const Addresswarninput = document.getElementById("address-warn");
+
+// const Meuccarrinho = Window.document.getElementById("meucarrinho");
+// const dentrodocarinho = Window.document.getElementById("submeucarrinho");
+// const Valortotal = Window.document.getElementById("valortotal");
+// const Fechar = Window.document.getElementById("Fechar");
+// const Finalizar = Window.document.getElementById("Finalizar");
+// const Quantidadecarinho = Window.document.getElementById("quantidadecarinho");
+// const Addressinput = Window.document.getElementById("address");
+// const Addresswarninput = Window.document.getElementById("address-warn");
+
+Meucarrinho.addEventListener("click", function () {
+  dentrodocarrinho.style.display = "block";
+});
+ww;
